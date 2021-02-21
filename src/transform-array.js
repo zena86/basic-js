5a409d20b15fe86bd757c074079dbcab48e13f8d
zena86 <76158579+zena86@users.noreply.github.com>
2021-02-21 09:06:04 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr) || arr == undefined) throw new Error('Bug');
+  
+  let result = [];
+  let pushItem = null;
+  
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] == '--discard-next'){
+      i++;
+      pushItem = null;
+    }else if(arr[i] == '--discard-prev'){
+      if(i > 0 && pushItem !== null)
+        result.pop();
+    }else if(arr[i] == '--double-next'){
+      if(i < arr.length-1 && arr[i+1] !== undefined)
+        result.push(arr[i+1]); 
+        pushItem = arr[i+1];
+    }else if(arr[i] == '--double-prev'){
+        if(i > 0 && pushItem !== null && pushItem !== undefined)
+          result.push(pushItem);
+    }else{
+      if(arr[i] !== undefined){
+        result.push(arr[i]);
+        pushItem = arr[i];
+      }   
+    }  
+  }
+  return result;
+}   
