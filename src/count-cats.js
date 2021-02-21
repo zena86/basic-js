f654aee7eafefe528656b3b9a3a1a393a20ffb41
zena86 <76158579+zena86@users.noreply.github.com>
2021-02-21 09:02:14 +0300

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let arrFlatCats = matrix.flat(1);
+  let result = 0;
+
+  for(let i=0; i<arrFlatCats.length; i++)
+  {
+    if(arrFlatCats[i] === "^^")
+      result += 1;
+  }
+  return result;
 };
