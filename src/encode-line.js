fd468ee42308703b94acd8fef3363fe14cd6cd9d
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-04 19:06:21 +0400

    feat: solve encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
+function encodeLine(str) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  let result = "";
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      result += (count > 1 ? count : "") + str[i];
+      count = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
