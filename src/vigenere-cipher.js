87143ddd56fddf62374dc8139776613d98c5ff1c
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-05 17:06:52 +0400

    feat: solve VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,90 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
+
+  constructor(isDirect = true){
+    this.isDirect = isDirect;
+  }
+  createTabulaRecta(){
+    const alphabet = ['A',  'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X','Y', 'Z'];
+    let tabulaRecta = [];
+    for(let i=0; i<alphabet.length; i++){
+      let tabulaRectaRow = [];
+      for(let j=0; j<alphabet.length; j++){
+        tabulaRectaRow.push(alphabet[(i+j)%alphabet.length]);
+      }
+      tabulaRecta.push(tabulaRectaRow);
+    }
+    return tabulaRecta;
+  }
+  encrypt(message, key) {
     // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if(arguments.length < 2 || message === undefined || message === null || message === '' || key === undefined || key === null || key === '') {
+      throw new Error('Incorrect arguments!');
+    }
+    let testMessage;
+    if(this.isDirect){
+      testMessage = message;
+    }else{
+      testMessage = message.split('').reverse().join('');
+    }
+    let strFromKey = '';
+    let result = '';
+    let tabulaRecta = this.createTabulaRecta();
+    let alphabet = tabulaRecta[0];
+    let upperMessage = testMessage.toUpperCase();
+    for(let i=0; i<message.length; i++){
+      strFromKey += (key[i%key.length]).toUpperCase();
+    }
+    let shift = 0;
+    for(let i=0; i<strFromKey.length; i++){
+      let messageLetter = upperMessage[i];
+      let keyLetter = strFromKey[i-shift];
+      let keyLetterIndex = alphabet.indexOf(keyLetter);
+      let messageLetterIndex = alphabet.indexOf(messageLetter);
+      if(messageLetterIndex < 0){
+        result += messageLetter;
+        shift++;
+      }else{
+        result += tabulaRecta[messageLetterIndex][keyLetterIndex];
+      } 
+    }
+    return result;
   }
 
-  decrypt() {
+  decrypt(encryptedMessage, key) {
     // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+        if(arguments.length < 2 || encryptedMessage === undefined || encryptedMessage === null || encryptedMessage === '' || key === undefined || key === null || key === '') {
+      throw new Error('Incorrect arguments!');
+    }
+    let testEncryptedMessage;
+    if(this.isDirect){
+      testEncryptedMessage = encryptedMessage;
+    }else{
+      testEncryptedMessage = encryptedMessage.split('').reverse().join('');
+    }
+    let result = '';
+    let tabulaRecta = this.createTabulaRecta();
+    let alphabet = tabulaRecta[0];
+    let strFromKey = '';
+    for(let i=0; i<encryptedMessage.length; i++){
+      strFromKey += (key[i%key.length]).toUpperCase();
+    }
+    let shift = 0;
+    for(let i=0; i<encryptedMessage.length; i++){
+      let encryptedMessageLetter = testEncryptedMessage[i];
+      let keyLetter = strFromKey[i-shift];
+      let keyLetterIndex = alphabet.indexOf(keyLetter);
+      let row = tabulaRecta[keyLetterIndex];
+      let encryptedMessageLetterIndex = row.indexOf(encryptedMessageLetter);
+      if(encryptedMessageLetterIndex < 0){
+        result += encryptedMessageLetter;
+        shift++;
+      }else{
+        result += alphabet[encryptedMessageLetterIndex];
+      }
+    }
+    return result;
   }
 }
 
